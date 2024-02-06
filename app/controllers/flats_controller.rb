fe26cbee6d51c37a932abb5b47fb5f25b6b0fbec
Ana Madeira <ana_madeira1988@outlook.com>
2024-02-06 18:22:10 +0000

    airbnb app knoty on fire

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,51 @@
+class FlatsController < ApplicationController
+  before_action :set_flat, only: %I[edit update destroy show]
+
+  def index
+    @flats = params[:search] ? Flat.where('name LIKE ?', "%#{params[:search]}%") : Flat.all
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new, status: 422
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @flat.update(flat_params)
+      redirect_to flats_path, status: :see_other
+    else
+      render :edit, status: 422
+    end
+  end
+
+  def destroy
+    if @flat.destroy
+      redirect_to flats_path, status: :see_other
+    else
+      render :index, status: 422
+    end
+  end
+
+  def show; end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name,:address, :description, :price_per_night, :number_of_guests, :img_url)
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+
+Flat.destroy_all
+
+5.times do
+  Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  img_url: 'https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+)
+end
